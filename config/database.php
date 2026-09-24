bf76306ea42e5e9c3d7c692953f673f142fc0dd4
yuhei-kawamata <y.kawamata7416@gmail.com>
2026-09-25 05:50:44 +0900

    To skip SSL

--- config/database.php
+++ config/database.php
@@ -60,6 +60,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                // SSL検証をスキップするための設定。500 サーバーエラーを防ぐために必要。
             ]) : [],
         ],
 
@@ -125,7 +127,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
