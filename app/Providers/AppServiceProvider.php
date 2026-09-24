c5043be67f1ff2289f03dd861ca1d1d2629e3260
yuhei-kawamata <y.kawamata7416@gmail.com>
2026-09-25 05:36:00 +0900

    Enable accsess https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 本番環境（production）ではHTTPSを強制する
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
